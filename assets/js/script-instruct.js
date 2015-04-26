4ae83984436ae72a144e68e0aa948f433554d21f
elizabethann <elizabethannmarrone@hotmail.com>
2015-04-26 15:34:03 -0700

    pre-resolution fixes

--- assets/js/script-instruct.js
+++ assets/js/script-instruct.js
@@ -1,221 +1,60 @@
 $(document).ready(function(){
-	$('.levels').slick({
-	  infinite: true,
-	  slidesToShow: 3,
-	  slidesToScroll: 2
-	});
 
-	var waypoint1 = new Waypoint({
-		element: document.getElementById('question1'),
+	var step1 = new Waypoint({
+		element: document.getElementById('step1'),
 		handler: function(direction) {
 			if(direction == 'down'){
-				$('#home-container').css('height', '3040');
-				$('#home-footer').css('top', '3040');
-				$('#tombstone').fadeIn();
-				$('#money').fadeIn();
-				$('#answer1').slideDown();
-				$('#answer1').css('position', 'fixed');
-				$('#answer1').css('top', '79');
-				$('#question1').css('position', 'fixed');
-				$('#question1').css('top', '63');
-				$('#selection1').css('height', '1600')
-				$('#question2').css('top', '2285');
-				$('#question3').css('top', '2490');
-				$('.animations').css('position', 'fixed');
-				$('.animations').css('top', '279');
-
+				$('#step1').css('color', 'AB1461');
 
 			} else {
-				$('#home-container').css('height', '1540');
-				$('#home-footer').css('top', '1540');
-				$('#tombstone').fadeOut();
-				$('#money').fadeOut();
-				$('#answer1').slideUp();
-				$('#answer1').css('position', 'absolute');
-				$('#answer1').css('top', '699');
-				$('#question1').css('position', 'absolute');
-				$('#question1').css('top', '684');
-				$('#selection1').css('height', '205')
-				$('#question2').css('top', '890');
-				$('.animations').css('position', 'relative');
-				$('#question3').css('top', '1095');
+				$('#step1').css('color', 'white');
 			}
 
 		},
-		offset: 80
+		offset: 150
 	})
 
-	var waypoint2 = new Waypoint({
-		element: document.getElementById('dummy-waypoint1'),
+	var step2 = new Waypoint({
+		element: document.getElementById('step2'),
 		handler: function(direction) {
 			if(direction == 'down'){
-				$('#home-container').css('height', '3730');
-				$('#home-footer').css('top', '3730');
-				$('#fitbit').fadeIn();
-				$('#signal').fadeIn();
-				$('#signal-heart').fadeIn();
-				$('#answer2').slideDown();
-				$('#answer2').css('position', 'fixed');
-				$('#answer2').css('top', '79');
-				$('#question2').css('position', 'fixed');
-				$('#question2').css('top', '63');
-				$('#selection2').css('height', '700')
-				$('.animations').hide();
-				$('#question3').css('top', '2985');
+				$('#step2').css('color', 'AB1461');
 
 			} else {
-				$('#home-container').css('height', '3040');
-				$('#home-footer').css('top', '3040');
-				$('#fitbit').fadeOut();
-				$('#signal').fadeOut();
-				$('#signal-heart').fadeOut();
-				$('#answer2').slideUp();
-				$('#answer2').css('position', 'absolute');
-				$('#answer2').css('top', '699');
-				$('#question2').css('position', 'absolute');
-				$('#question2').css('top', '2285');
-				$('#selection2').css('height', '205');
-				$('.animations').show();
-				$('#question3').css('top', '2290');
+				$('#step2').css('color', 'white');
 			}
 
 		},
-		offset: 80
+		offset: 150
 	})
 
-	var waypoint3 = new Waypoint({
-		element: document.getElementById('dummy-waypoint2'),
+	var step3 = new Waypoint({
+		element: document.getElementById('step3'),
 		handler: function(direction) {
 			if(direction == 'down'){
-				$('#home-container').css('height', '3800');
-				$('#home-footer').css('top', '3800');
-				$('#care-to').fadeIn()
-				$('#exorcise-big').fadeIn();
-				$('#selection3').children('#sign-up-button').fadeIn()
-				$('#answer3').slideDown();
-				$('#answer3').css('position', 'fixed');
-				$('#answer3').css('top', '79');
-				$('#question3').css('position', 'fixed');
-				$('#question3').css('top', '63');
-				$('#selection3').css('height', '800')
+				$('#step3').css('color', 'AB1461');
 
 			} else {
-				$('#home-container').css('height', '3730');
-				$('#home-footer').css('top', '3730');
-				$('#care-to').fadeOut()
-				$('#exorcise-big').fadeOut();
-				$('#selection3').children('#sign-up-button').fadeOut()
-				$('#answer3').slideUp();
-				$('#answer3').css('position', 'absolute');
-				$('#answer3').css('top', '2985');
-				$('#question3').css('position', 'absolute');
-				$('#question3').css('top', '2985');
-				$('#selection3').css('height', '205')
+				$('#step3').css('color', 'white');
 			}
 
 		},
-		offset: 80
+		offset: 150
 	})
 
-	var moneyFly = new Waypoint({
-		element: document.getElementById('dummy-waypoint-tombstone'),
+	var step4 = new Waypoint({
+		element: document.getElementById('step4'),
 		handler: function(direction) {
 			if(direction == 'down'){
-				$('#tombstone').css('position', 'fixed');
-				$('#tombstone').css('top', '480');
-			} else {
-				$('#tombstone').css('position', 'relative');
-				$('#tombstone').css('top', '600');
-			}
-		}
-	})
-
-	var tombstoneRelease = new Waypoint({
-		element: document.getElementById('dummy-waypoint-tombstone-release'),
-		handler: function(direction) {
-			if(direction == 'down'){
-				$('#tombstone').css('position', 'relative');
-				$('#tombstone').css('top', '770');
-				$('#money').hide();
+				$('#step4').css('color', 'AB1461');
 
 			} else {
-				$('#tombstone').css('position', 'fixed');
-				$('#tombstone').css('top', '480');
-				$('#money').show();
+				$('#step4').css('color', 'white');
 			}
-		}
+
+		},
+		offset: 150
 	})
 
-	var miles = new Waypoint({
-		element: document.getElementById('dummy-waypoint-miles'),
-		handler: function(direction) {
-			if(direction == 'down'){
-				$('#miles-text').slideDown();
-				$('#wall-box').slideDown();
-
-			} else {
-				$('#miles-text').slideUp();
-				$('#wall-box').slideUp();
-			}
-		}
-	})
-
-	var miles = new Waypoint({
-		element: document.getElementById('dummy-waypoint-miles-slide'),
-		handler: function(direction) {
-			if(direction == 'down'){
-				$('#miles-text').offset({left: 940});
-
-			} else {
-				$('#miles-text').offset({left: 400});
-			}
-		}
-	})
-
-	var lives = new Waypoint({
-		element: document.getElementById('dummy-waypoint-lives'),
-		handler: function(direction) {
-			if(direction == 'down'){
-				$('#lives-home').fadeIn(300);
-				$('#miles-text').css('opacity', '0');
-
-
-			} else {
-				$('#lives-home').fadeOut(300);
-				$('#miles-text').css('opacity', '1');
-			}
-		}
-	})
-
-	var livesEnd = new Waypoint({
-		element: document.getElementById('dummy-waypoint-lives-end'),
-		handler: function(direction) {
-			if(direction == 'down'){
-				$('#lives-home').fadeOut(300);
-				$('#wall-box').fadeOut(300);
-
-
-			} else {
-				$('#lives-home').fadeIn(300);
-				$('#wall-box').fadeIn(300);
-
-			}
-		}
-	})
-
-	$("#selection3").children( "#sign-up-button" ).mouseenter(function () {
-		$('#exorcise-big').css('color', 'AB1461');
-	})
-
-	$("#selection3").children( "#sign-up-button" ).mouseleave(function () {
-		$('#exorcise-big').css('color', 'white');
-	})
-
-	$('#lives').children(".triangle-up").click(function () {
-		// var currLives = parseInt($('#lives').children.('.resource-number').text());
-		// $('#lives').children.('.resource-number').text(currLives + 1);
-		console.log("hello");
-
-	});
 				
 });
